6cf0653a8987474ab657640bcf34305bc606d1c2
martinascharrrer <martina.scharrer@outlook.com>
2021-12-12 18:24:36 +0100

    Retrieve dependency information
    Use dependency-cruise API to parse all files and
    get their dependencies.

--- server/index.ts
+++ server/index.ts
@@ -3,9 +3,12 @@ import { normalize } from 'path';
 import { parse } from 'vue-eslint-parser';
 // eslint-disable-next-line import/extensions,import/no-unresolved
 import { ESLintProgram } from 'vue-eslint-parser/ast/nodes';
+import {
+  cruise, IReporterOutput, ICruiseOptions, IDependency,
+} from 'dependency-cruiser';
 
 import { getProps } from './analyzer';
-import { getVueFilePaths } from './files';
+import { getFileNameFromPath, getVueFilePaths } from './files';
 
 const parserOption = {
   ecmaVersion: 2018,
@@ -17,28 +20,50 @@ type VueComponent = {
   fileName: string,
   file: string,
   props: string | null,
+  dependencies: IDependency[],
 }
+
+const DEPCRUISE_OPTIONS: ICruiseOptions = {
+  includeOnly: 'src',
+};
+
+const cruiseComponents = (components: string[]):IReporterOutput | null => {
+  let cruiseResult: IReporterOutput | null = null;
+  try {
+    cruiseResult = cruise(
+      components,
+      DEPCRUISE_OPTIONS,
+    );
+  } catch (error) {
+    console.error(error);
+  }
+  return cruiseResult;
+};
 
 (async () => {
   const paths = await getVueFilePaths(process.cwd());
   console.log(`Found ${paths.length} Vue components`);
 
-  const components: VueComponent[] = [];
-  paths.forEach((path: string) => {
-    const pathNormalized = normalize(path);
-    const file = readFileSync(pathNormalized, { encoding: 'utf-8' });
-    const fileName = pathNormalized.replace(normalize(process.cwd()), '');
-    const esLintProgram: ESLintProgram = parse(file, parserOption);
-    const props = esLintProgram.tokens ? getProps(esLintProgram.tokens) : null;
+  const cruiseResult = cruiseComponents(paths);
+  if (cruiseResult && 'modules' in cruiseResult.output) {
+    const components: VueComponent[] = [];
+    cruiseResult.output.modules.forEach((module) => {
+      const pathNormalized = normalize(module.source);
+      const file = readFileSync(pathNormalized, { encoding: 'utf-8' });
+      const fileName = getFileNameFromPath(pathNormalized);
+      const esLintProgram: ESLintProgram = parse(file, parserOption);
+      const props = esLintProgram.tokens ? getProps(esLintProgram.tokens) : null;
 
-    components.push({
-      fullPath: pathNormalized,
-      file,
-      fileName,
-      props,
+      components.push({
+        fullPath: pathNormalized,
+        file,
+        fileName,
+        props,
+        dependencies: module.dependencies,
+      });
+      console.log('component: ', fileName, ', props: ', props, 'deps: ', module.dependencies.length);
     });
 
-    console.log('component: ', fileName, ', props: ', props);
-  });
-  console.log(`Parsed ${components.length} Vue components`);
+    console.log(`Parsed ${components.length} Vue components`);
+  }
 })();
