c2473397b548c8e47923358b85453765675963b9
Martina Scharrer <martina.scharrer@karriere.at>
2022-01-03 12:36:48 +0100

    Use ts for cli package and introduce --dir flag

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
-"use strict";
-exports.__esModule = true;
-var commander_1 = require("commander");
-commander_1.program
+
+import { program } from 'commander';
+
+program
     .description('React Bratus CLI')
     .option('--dir [dir]', 'specify the directory that should be analyzed', 'src')
     .parse();
-var dir = commander_1.program.opts().dir;
+
+const dir = program.opts().dir;
+
 console.log(dir, 'HELLO!');
