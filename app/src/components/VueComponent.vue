15e530a90d7a62350462201de3dbc8635e503732
Martina Scharrer <martina.scharrer@karriere.at>
2022-01-17 13:19:17 +0100

    Improve data fetching and representation
    - Add fetch composable for unified loading and error states.
    - Render components information in a grid view.

---
+++ app/src/components/VueComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="component">
+      <h3>{{ component.fileName }}</h3>
+      <p v-if="component.props && component.props.length > 0">
+          <em>props:</em> {{ component.props.map(prop => prop.name).join(', ') }}
+      </p>
+      <p v-if="component.events && component.events.length > 0">
+          <em>events:</em> {{ component.events.map(event => event.name).join(', ') }}
+      </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'VueComponent',
+  props: {
+    component: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.component {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background: #c8e8e0;
+    border-radius: 20px;
+    width: 16vw;
+    padding: 2rem;
+    flex: 1 15vw;
+}
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,29 +1,58 @@
 <template>
-  <div class="home">
-     <ul>
-         <li v-for="item in data" :key="item.name">
-             {{ item.name }}: {{ item.props.map(i => i.name).join() }}
-         </li>
-     </ul>
-  </div>
+    <div class="home">
+        <p v-if="isLoading">... loading</p>
+        <p v-else-if="error">Something went wrong parsing your project.</p>
+        <div v-else>
+            <p>Total amount of components: {{ data.length }}</p>
+            <div class="component-list">
+                <vue-component
+                    v-for="component in data"
+                    :key="component.name"
+                    :component="component"
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, watchEffect } from 'vue';
+import { defineComponent } from 'vue';
 import { getParsedData } from '@/api';
+
+import VueComponent from '@/components/VueComponent.vue';
+
+import { useFetch } from '@/composables/fetch';
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    VueComponent,
+  },
   setup() {
-    console.log('setup hook');
-    const data = ref(null);
-    getParsedData().then((result) => { data.value = result; });
+    const {
+      data,
+      getData,
+      isLoading,
+      error,
+    } = useFetch(getParsedData);
 
-    watchEffect(() => console.log('data', data.value));
+    getData();
 
     return {
       data,
+      isLoading,
+      error,
     };
   },
 });
 </script>
+<style scoped lang="scss">
+.component-list {
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 80vw;
+    justify-content: space-between;
+    gap: 2vw;
+}
+</style>
