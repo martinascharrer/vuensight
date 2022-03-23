d837d609382fea82fa00cdc9fc28620304c6270b
Martina Scharrer <martina.scharrer@karriere.at>
2022-03-23 22:40:08 +0100

    Set up monorepo with npm workspaces

---
+++ packages/server/src/index.ts
@@ -0,0 +1,22 @@
+import * as history from 'connect-history-api-fallback';
+import * as express from 'express';
+import { join } from 'path';
+
+import { parse } from '@vue-component-insight/parser/dist';
+
+export const startServer = async (directory: string) => {
+    const app = express();
+
+    app.get('/parse-result', async (request, response) => {
+        const parseResult = await parse(directory);
+        response.json(parseResult);
+    });
+
+    app.use(history());
+
+    app.use(express.static(join(__dirname, '../../app/dist')));
+
+    app.listen(4444, () => {
+        console.log('server is listening on port 4444');
+    });
+};
